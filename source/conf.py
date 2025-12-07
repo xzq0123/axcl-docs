fcd80fecf4020b897fcc7fc9be36e75b7d0e3504
xzq <1154078186@qq.com>
2025-12-07 21:45:54 +0800

    xinjian

--- source/conf.py
+++ source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'axcl'
+project = '芯茧'
 copyright = '2025, xinjian'
 author = 'xinjian'
 release = '1.0.0'
@@ -14,7 +14,10 @@ release = '1.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'recommonmark',
+    'sphinx_markdown_tables'
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +27,6 @@ language = 'zh_CN'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+# https://sphinx-themes.org/
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
--- source/index.rst
+++ source/index.rst
@@ -3,15 +3,15 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-axcl documentation
+芯茧算力卡
 ==================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: 目录
+
+   about
 
 Add your content using ``reStructuredText`` syntax. See the
 `reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
 documentation for details.
-
-
-.. toctree::
-   :maxdepth: 2
-   :caption: Contents:
-
